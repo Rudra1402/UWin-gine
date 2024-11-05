f1f505de2ec99d01280acd48bebc9176c42396d8
Vaibhav <koratvaibhav123@gmail.com>
2024-11-05 13:14:51 -0500

    Modularize chat page code.

---
+++ client/src/apis/chatApis.js
@@ -0,0 +1,41 @@
+import { toast } from "react-toastify";
+
+export const sendMessageApi = async (requestBody) => {
+    try {
+        const response = await fetch('http://localhost:8000/user/chat/sd', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestBody)
+        });
+
+        if (!response.ok) {
+            toast.error('Failed to send message.', {
+                autoClose: 2000
+            });
+            return { message: 'We are currently experiencing technical difficulties. Our team is actively working on resolving the issue. Please try again shortly.' };
+                }
+
+        const data = await response.json();
+
+        if(data?.status === "error") {
+            toast.error('Server processing error.', {
+                autoClose: 2000
+            });
+            return { message: 'Error retrieving response from server.' }; 
+        }
+
+        toast.success('Message sent successfully!', {
+            autoClose: 2000
+        });
+
+        return data; // Returns the response data from the server
+    } catch (error) {
+        console.error('Failed to send message:', error);
+        toast.error('Communication error.', {
+            autoClose: 2000
+        });
+        return { message: 'Error retrieving response from server.' };
+    }
+};
