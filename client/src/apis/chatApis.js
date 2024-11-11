1c87841c68a8b7d0754d60503e2d227a123e6da3
Rudra Patel <rudra.190410116095@gmail.com>
2024-11-11 17:30:29 -0500

    integrated chat history

--- client/src/apis/chatApis.js
+++ client/src/apis/chatApis.js
@@ -1,9 +1,30 @@
 import api from "@/axios/axios";
+import {v4 as uuidv4} from 'uuid';
 import { toast } from "react-toastify";
+
+export const fetchChatHistoryApi = async (threadId) => {
+    try {
+        const response = await api.get(`/user/chat/history/${threadId}`);
+
+        if (response.data?.status !== "success") {
+            throw new Error(`Error fetching chat history: ${response.statusText}`);
+        }
+
+        // Access the data directly from response.data
+        return response.data.data;
+    } catch (error) {
+        console.error('Failed to fetch chat history:', error);
+        return []; // Return an empty array if there's an error
+    }
+};
 
 export const sendMessageApi = async (requestBody) => {
     try {
         // Make the POST request with Axios
+        if (requestBody.thread_id == "") {
+            requestBody = {...requestBody, thread_id: uuidv4().replace(/-/g, '').substring(0, 24)}
+        }
+        console.log(requestBody)
         const response = await api.post('/user/chat', requestBody);
 
         // Check if the response indicates an error status in the data
