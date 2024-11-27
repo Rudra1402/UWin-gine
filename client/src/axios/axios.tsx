b4a3d9349718b43825ff9c27dee0848f24a55553
Rudra Patel <rudra.190410116095@gmail.com>
2024-11-27 15:10:15 -0500

    IP changed to Elastic IP

--- client/src/axios/axios.tsx
+++ client/src/axios/axios.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // const uri = "http://54.147.167.63:8000"
-// const uri = "http://3.230.205.205:8000"
-const uri = "http://localhost:8000"
+const uri = "http://3.230.205.205:8000"
+// const uri = "http://localhost:8000"
 
 const api = axios.create({
     baseURL: uri,
